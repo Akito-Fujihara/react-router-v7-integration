cc520fcc7013dc7626b866d111f09b27b72a2292
Akito-Fujihara <1998922akito@gmail.com>
2025-04-01 00:45:47 +0900

    home 画面の追加 & 必要な component の追加

---
+++ app/common/home.tsx
@@ -0,0 +1,28 @@
+import type { Route } from "./+types/home";
+import { Link } from "react-router";
+import { Button } from "@/components/ui/button";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Home" },
+    { name: "description", content: "Home" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="p-4 rounded-lg border">
+        <h1 className="mb-4 text-xl font-bold text-center">Home</h1>
+        <div className="space-x-2 flex justify-center">
+          <Link to="/account/login">
+            <Button>ログイン</Button>
+          </Link>
+          <Link to="/account/signup">
+            <Button>新規作成</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
